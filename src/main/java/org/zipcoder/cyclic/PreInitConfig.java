21c38bb84735210561c5d4f1f76a5e55bf4964ba
zipCoder933 <sam.p.w524@gmail.com>
2025-10-19 01:13:36 -0600

    v

--- src/main/java/org/zipcoder/cyclic/PreInitConfig.java
+++ src/main/java/org/zipcoder/cyclic/PreInitConfig.java
@@ -1,9 +1,8 @@
-package org.zipcoder.cyclic.blocks;
+package org.zipcoder.cyclic;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
 import com.electronwill.nightconfig.toml.TomlFormat;
 import net.minecraftforge.fml.loading.FMLPaths;
-import org.zipcoder.cyclic.Cyclic;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -15,7 +14,7 @@ public class PreInitConfig {
         try {
 
             Path path = FMLPaths.CONFIGDIR.get();
-            File configFile = new File(path.toFile(), "cyclic-pre-init.toml");
+            File configFile = new File(path.toFile(), "cyclic-common.toml");
             try (FileConfig config = FileConfig.builder(configFile, TomlFormat.instance()).build()) {
                 if (configFile.exists()) {
                     loadConfig(config);
